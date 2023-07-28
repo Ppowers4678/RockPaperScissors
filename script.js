b1f771c0b5fd81d3c96028f655089df7231d2dce
Ppowers4678 <135461461+Ppowers4678@users.noreply.github.com>
2023-07-28 08:24:54 -0500

    Have added computer response for all choices

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
- function randomNum() {
-    return Math.floor(Math.random() * 100)
- };
- const foo = randomNum() 
+function randomNum() {
+       return Math. floor (Math.random () * 100)
+       };
+       const foo = randomNum ()
+       let rockBtn = document.getElementById("Rock!");
+       let paperBtn = document.getElementById("Paper!")
+       let scissorsBtn = document.getElementById("Scissors!")
 
- let button = document.querySelector("button");
+      
+       
+       function choiceForRock() {
+              if (randomNum() <= 33) {
+                     console.log("Rock!")
+                     alert("Computer Chose Rock! Draw!") }
+              else if (randomNum() >= 34 && randomNum()<= 66) {
+                     console.log("Paper!")
+                     alert("Computer Chose Paper! You Lose!")}
+              else { (randomNum()>= 67) 
+                     console.log("Scissors!")
+                     alert("Computer Chose Scissors! You Win!")
+              
+              }}
+              rockBtn.addEventListener("click", () => {
+                     choiceForRock() 
+                     }
+              )
 
- function getComputerChoice() {
-
-
-    {if (foo <= 33) {
-        console.log("Rock!")
- }   else if (foo >= 34 && foo<= 66)  { 
-        console.log("Paper!")
- }  else
-        console.log("Scissors!")
- }};
-
- const foo2 = getComputerChoice();
-
- button.addEventListener("click", foo2);
-
+        function choiceForPaper() {
+                 if (randomNum() <= 33) {
+                     console.log("Rock!")
+                       alert("Computer Chose Rock! You Win!") }
+                  else if (randomNum() >= 34 && randomNum()<= 66) {
+                        console.log("Paper!")
+                     alert("Computer Chose Paper! Draw!")}
+                  else { (randomNum()>= 67) 
+                           console.log("Scissors!")
+                         alert("Computer Chose Scissors! You Lose!")
+                     
+                    }}
+                    paperBtn.addEventListener("click", () => {
+                           choiceForPaper() 
+                           }
+                    )
+         function choiceForScissors() {
+                 if (randomNum() <= 33) {
+                         console.log("Rock!")
+                         alert("Computer Chose Rock! You Lose!") }
+                  else if (randomNum() >= 34 && randomNum()<= 66) {
+                         console.log("Paper!")
+                      alert("Computer Chose Paper! You Win!")}
+                  else { (randomNum()>= 67) 
+                          console.log("Scissors!")
+                        alert("Computer Chose Scissors! Draw!")
+                     
+                    }}
+                   scissorsBtn.addEventListener("click", () => {
+                           choiceForScissors() 
+                           }
+                    )
  
