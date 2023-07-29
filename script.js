ca31eee6128815c94ab9ed2ce8d6ca8f700eeb1c
Ppowers4678 <135461461+Ppowers4678@users.noreply.github.com>
2023-07-29 08:11:28 -0500

    Have added player choice options/working on count

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function randomNum() {
        return Math. floor (Math.random () * 100)
        };
-       const foo = randomNum ()
-       let rockBtn = document.getElementById("Rock!");
-       let paperBtn = document.getElementById("Paper!")
-       let scissorsBtn = document.getElementById("Scissors!")
-
+       let number = randomNum ()
+       let rockBtn = document.getElementById("Rock")
+       let paperBtn = document.getElementById("Paper")
+       let scissorsBtn = document.getElementById("Scissors")
       
        
        function choiceForRock() {
@@ -42,19 +41,46 @@ function randomNum() {
                            }
                     )
          function choiceForScissors() {
-                 if (randomNum() <= 33) {
+                 if (number <= 33) {
                          console.log("Rock!")
-                         alert("Computer Chose Rock! You Lose!") }
-                  else if (randomNum() >= 34 && randomNum()<= 66) {
+                         alert("Computer Chose Rock! You Lose!")
+                            return "loss" }
+                  else if (number >= 34 && number <= 66) {
                          console.log("Paper!")
-                      alert("Computer Chose Paper! You Win!")}
-                  else { (randomNum()>= 67) 
+                      alert("Computer Chose Paper! You Win!")
+                            return "win" }
+                  else { (number>= 67) 
                           console.log("Scissors!")
                         alert("Computer Chose Scissors! Draw!")
+                        return "draw"
                      
                     }}
                    scissorsBtn.addEventListener("click", () => {
-                           choiceForScissors() 
-                           }
-                    )
+                          choiceForScissors()
+                             if (number <= 33) {
+                                   lossCounter()
+                             } else if (number >= 34 && number<=66) {
+                                   winCounter()
+                             } else {drawCounter() }});
+                           
+                    
+              function lossCounter() {
+                     let losses = document.querySelectorAll("loss")
+                     let count = losses.length
+                     console.log(`You have ${count} losses!`)
+                     return count
+
+              }
+              function winCounter() {
+                     const wins = document.querySelectorAll("win")
+                     const count = wins.length
+                     console.log( `You have ${count} wins!`)
+                     return count
+              }
+              function drawCounter() {
+                     const draws = document.querySelectorAll("draw")
+                     const count = draws.length
+                     console.log(`You have ${count} draws!`)
+                     return count
+              }
  
